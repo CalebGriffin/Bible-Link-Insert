8c58ee5c996c8c08fc4485858667e812233a407b
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2023-11-08 15:52:47 +0000

    Created Plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// This adds an editor command that replaces the selected text with a link to the corresponding Bible verse.
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'insert-bible-link',
+			name: 'Insert Bible Link',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				// Get the selected text
+				const selectedText = editor.getSelection();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const linkText = this.getBibleLink(selectedText);
+
+				if (linkText === "") {
+					return;
 				}
+
+				// Replace the selected text with the link text
+				editor.replaceSelection(linkText);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'insert-bible-link-with-olivetree',
+			name: 'Insert Bible Link with OliveTree',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				// Get the selected text
+				const selectedText = editor.getSelection();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+				const linkText = this.getBibleLink(selectedText);
+
+				if (linkText === "") {
+					return;
+				}
+
+				const olivetreeLinkText = this.getOlivetreelink(selectedText);
+
+				if (olivetreeLinkText === "") {
+					return;
+				}
+
+				// Replace the selected text with the link text
+				editor.replaceSelection(linkText + " " + olivetreeLinkText);
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	getBibleLink(selectedText:string):string {
+		// If the selected text is empty, show an error message
+		if (selectedText === '') {
+			new Notice('No text selected!');
+			return "";
+		}
+
+		// Use regex to split the text by the space that has a letter before it and a number after it
+		const [book, chapterAndVerse] = selectedText.split(/(?<=[a-zA-Z])\s(?=\d)/);
+
+		// If the split failed, show an error message
+		if (book === undefined || chapterAndVerse === undefined) {
+			new Notice('Invalid text selected!');
+			return "";
+		}
+
+		// Split the rest by the colon
+		let [chapter, verses] = chapterAndVerse.split(':');
+
+		// If the split failed, show an error message
+		if (chapter === undefined || verses === undefined) {
+			new Notice('Invalid text selected!');
+			return "";
+		}
+
+		// If the verses contains a dash, split it by the dash
+		if (verses.includes('-')) {
+			verses = verses.split('-')[0];
+
+			// If the split failed, show an error message
+			if (verses === undefined) {
+				new Notice('Invalid text selected!');
+				return "";
+			}
+		}
+
+		// Create the link text
+		return `[[${book} ${chapter}#${verses}|${selectedText}]]`;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	getOlivetreelink(selectedText:string):string {
+		// Use regex to split the text by the space that has a letter before it and a number after it
+		let [book, chapterAndVerse] = selectedText.split(/(?<=[a-zA-Z])\s(?=\d)/);
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		// Remove leading and trailing whitespace from book
+		book = book.trim();
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		// Make the book URL friendly
+		book = book.replace(' ', '%20');
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		// Split the rest by the colon
+		let [chapter, verses] = chapterAndVerse.split(':');
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+		// If the verses contains a dash, split it by the dash
+		if (verses.includes('-')) {
+			verses = verses.split('-')[0];
+		}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// Create the link text
+		return `[(OTL)](olivetree://bible/${book}.${chapter}.${verses})`;
 	}
 }
